7c1ebf3b4f73f9692bf1ae89a81324e3ec30ccbe
AlbertLlebaria <Allebaria94@gmail.com>
2019-02-27 01:38:51 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-DEBUG = True # Turns on debugging features in Flask
+DEBUG = Flase # Turns on debugging features in Flask
 BCRYPT_LOG_ROUNDS = 12 # Configuration for the Flask-Bcrypt extension
 MAIL_FROM_EMAIL = 'robert@example.com'  # For use in application emails

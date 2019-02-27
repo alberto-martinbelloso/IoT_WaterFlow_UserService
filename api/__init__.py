9fa793c0d5c3ab288890c77ec9a9b31183ceffd9
Albert <Allebaria94@gmail.com>
2019-02-28 00:09:49 +0100

    Added JWT

--- api/__init__.py
+++ api/__init__.py
@@ -1,15 +1,35 @@
 from flask import Flask
+from api.auth import authenticate, identity
 from api.mongo import mongo_blueprint
 from api.devices import devices_blueprint
+from flask_jwt import JWT, jwt_required, current_identity
+import os
 
+secret = 'super-secret'
+try:
+    if os.environ["DEPLOY"]:
+        host = os.environ["SECRET"]
+except:
+    print('INFO | Development secret key')
 # create and configure the app
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secret
+
 app.register_blueprint(mongo_blueprint)
+app.register_blueprint(devices_blueprint)
 app.register_blueprint(devices_blueprint)
 
 
+# JWT
+jwt = JWT(app, authenticate, identity)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity

ae517e30e8c54613a0327e1df2e546f87a1eb159
Albert <Allebaria94@gmail.com>
2019-02-28 00:46:08 +0100

    - removed auth - added influxdb

--- api/__init__.py
+++ api/__init__.py
@@ -4,55 +4,57 @@ from werkzeug.security import safe_str_cmp
 from api.model_user import User
 from api.mongo import mongo_blueprint, db
 from api.devices import devices_blueprint
+from api.influx import influx_blueprint
+
 from flask_jwt import JWT, jwt_required, current_identity
 import os
 
-secret = 'super-secret'
-try:
-    if os.environ["DEPLOY"]:
-        secret = str(os.environ["SECRET"])
-except ValueError:
-    print('INFO | Development secret key')
-
-
-def find_user(u, username):
-    for user in u:
-        if user["username"].encode('utf-8') == username.encode('utf-8'):
-            return user
-
-
-def authenticate(username, password):
-    user = find_user(_users, username)
-    if user and safe_str_cmp(user["password"].encode('utf-8'), password.encode('utf-8')):
-        return User(user)
-
-
-def identity(payload):
-    user_id = payload['identity']
-    return _collection.find_one({'_id': ObjectId(user_id)})
+# secret = 'super-secret'
+# try:
+#     if os.environ["DEPLOY"]:
+#         secret = str(os.environ["SECRET"])
+# except ValueError:
+#     print('INFO | Development secret key')
+#
+#
+# def find_user(u, username):
+#     for user in u:
+#         if user["username"].encode('utf-8') == username.encode('utf-8'):
+#             return user
+#
+#
+# def authenticate(username, password):
+#     user = find_user(_users, username)
+#     if user and safe_str_cmp(user["password"].encode('utf-8'), password.encode('utf-8')):
+#         return User(user)
+#
+#
+# def identity(payload):
+#     user_id = payload['identity']
+#     return _collection.find_one({'_id': ObjectId(user_id)})
 
 
 # create and configure the app
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = secret
+# app.config['SECRET_KEY'] = secret
 
 app.register_blueprint(mongo_blueprint)
 app.register_blueprint(devices_blueprint)
-app.register_blueprint(devices_blueprint)
+app.register_blueprint(influx_blueprint)
 
 _collection = db["users"]
 _users = _collection.find({})
 
 # JWT
-jwt = JWT(app, authenticate, identity)
+# jwt = JWT(app, authenticate, identity)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-
-@app.route('/protected')
-@jwt_required()
-def protected():
-    return '%s' % current_identity
+#
+# @app.route('/protected')
+# @jwt_required()
+# def protected():
+#     return '%s' % current_identity
---
+++ api/influx.py
@@ -0,0 +1,26 @@
+from influxdb import InfluxDBClient
+from flask import Blueprint
+import os
+
+influx_blueprint = Blueprint('Influxdb', __name__)
+host = "localhost"
+port = 8086
+database = "waterflow"
+
+try:
+    if os.environ["DEPLOY"]:
+        host = os.environ["INFLUX_HOST"]
+except Exception as e:
+    print("Running on develop environment")
+
+InfluxDB = InfluxDBClient(host=host, port=port, database=database)
+
+
+def get_measurements(dev_id, f, t):
+    query = "SELECT * FROM flow WHERE time >= %s and time <=%s and devid <> \"%s\"" % (f, t, dev_id)
+    results = InfluxDB.query(query)
+    points = results.get_points()
+    measurements = ""
+    for point in points:
+        measurements += "%s, %s, %i \n" % (point['time'], point['devid'], point["value"])
+    return measurements

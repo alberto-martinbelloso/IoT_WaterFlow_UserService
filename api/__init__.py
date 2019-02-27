71bb70e4d33c6e0dbfacbca2b90aa856fceb0538
Albert <Allebaria94@gmail.com>
2019-02-28 00:15:24 +0100

    - Removed all jwt from auth and moved into init

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
-from api.auth import authenticate, identity
-from api.mongo import mongo_blueprint
+from api.mongo import mongo_blueprint, db
 from api.devices import devices_blueprint
-from flask_jwt import JWT, jwt_required, current_identity
+from flask_jwt import JWT, jwt_required
 import os
 
 secret = 'super-secret'
@@ -11,6 +10,25 @@ try:
         host = os.environ["SECRET"]
 except:
     print('INFO | Development secret key')
+
+
+def find_user(u, username):
+    for user in u:
+        if user["username"].encode('utf-8') == username.encode('utf-8'):
+            return user
+
+
+def authenticate(username, password):
+    user = find_user(_users, username)
+    if user and safe_str_cmp(user["password"].encode('utf-8'), password.encode('utf-8')):
+        return User(user)
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return _collection.find_one({'_id': ObjectId(user_id)})
+
+
 # create and configure the app
 
 app = Flask(__name__)
@@ -20,6 +38,8 @@ app.register_blueprint(mongo_blueprint)
 app.register_blueprint(devices_blueprint)
 app.register_blueprint(devices_blueprint)
 
+_collection = db["users"]
+_users = _collection.find({})
 
 # JWT
 jwt = JWT(app, authenticate, identity)

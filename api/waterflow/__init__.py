892cd3ba94dacfed0d2f88d69cde3b8e74bd58ab
Albert <Allebaria94@gmail.com>
2019-02-28 12:27:02 +0100

    Added waterflow module to get data from device

--- api/waterflow/__init__.py
+++ api/waterflow/__init__.py
@@ -1,10 +1,39 @@
 from flask import Blueprint
 from api.waterflow.influx import get_measurements
+from flask import request, abort
+from datetime import datetime
+
+import calendar
+import time
 
 waterflow_blueprint = Blueprint('waterflow', __name__)
 
 
-@waterflow_blueprint.route('/waterflow/device_id=<dev_id>&from=<f>&to=<t>')
-def water(dev_id=None, f=None, t=None):
-    measures = get_measurements(dev_id, f, t)
-    return measures
+@waterflow_blueprint.route('/waterflow/<device_id>')
+def water(device_id=None):
+    f = request.args.get('from')
+    t = request.args.get('to')
+    try:
+        if f is None:
+            return abort(400)
+        else:
+            f = datetime.utcfromtimestamp(int(f))
+            if t is None:
+                t = datetime.utcfromtimestamp(dt2ts(datetime.now()))
+            else:
+                t = datetime.utcfromtimestamp(int(t))
+
+            measures = get_measurements(device_id, f, t)
+            return measures
+    except Exception as e:
+        return abort(400)
+
+
+def dt2ts(dt):
+    """Converts a datetime object to UTC timestamp
+
+    naive datetime will be considered UTC.
+
+    """
+
+    return calendar.timegm(dt.utctimetuple())

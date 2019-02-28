9a404b9985b84dfb25f232cfb3249bc36943d6a2
Albert <Allebaria94@gmail.com>
2019-02-28 01:08:06 +0100

    back to devices

--- api/__init__.py
+++ api/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
-from api.mongo import mongo_blueprint
+from api.mongo import mongo_blueprint, db
 from api.devices import devices_blueprint
+
 
 # create and configure the app
 
 app = Flask(__name__)
+
 app.register_blueprint(mongo_blueprint)
 app.register_blueprint(devices_blueprint)
-
-
 
 @app.route("/")
 def hello():
     return "Hello World!"
+

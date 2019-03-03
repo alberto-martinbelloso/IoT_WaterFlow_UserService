6e560729dc68061bd532c3f98ed6b6f1c0e20efd
AlbertLlebaria <Allebaria94@gmail.com>
2019-03-03 15:18:36 +0100

    Alarms Module: (#8)
    - Implemented GET /alarms.
    - JWT required
    - Distinction between normal user and admin

---
+++ api/alarms/alarms.py
@@ -0,0 +1,16 @@
+from api.mongo import db
+
+
+def get_alarms_from_device(device_id):
+    alarms = db['alarms'].find({
+        'device_id': device_id
+    }, {'_id': False})
+    return list(alarms)
+
+
+def get_all_alarms(f):
+    if f is None:
+        f = 0
+    alarms = db['alarms'].find({}, {'_id': False}).skip(f).limit(20)
+    print(alarms)
+    return list(alarms)
